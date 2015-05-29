2b42e0999fb00fb5f669877f084b7942fe0d5e77
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-05-30 00:11:19 +0100

    working routing

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,43 @@
 'use strict';
 
 import React from 'react';
-import Feed from './components/Feed';
 
-React.render(<Feed />, document.getElementById('app'));
+//pages
+import homePage from './pages/home';
+import settingsPage from './pages/settings';
+import crimeMapper from './pages/map';
+import crimeAlarm from './pages/alarm';
+
+//router
+import Router from 'react-router';
+
+var Route = Router.Route,
+    DefaultRoute = Router.DefaultRoute,
+    NotFoundRoute = Router.NotFoundRoute,
+    Redirect = Router.Redirect,
+    RouteHandler = Router.RouteHandler
+
+var App = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>Co-pilot</h1>
+                <RouteHandler/>
+            </div>
+        )
+    }
+});
+
+var routes = (
+<Route handler={App} path="/">
+    <DefaultRoute handler={homePage} />
+    <Route name="about" handler={settingsPage} />
+    <Route name="crimeMapper" handler={crimeMapper} />
+    <Route name="crimeAlarm" handler={crimeAlarm} />
+</Route>
+
+);
+
+Router.run(routes, function (Handler) {
+    React.render(<Handler />, document.body);
+});
---
+++ src/js/pages/map.js
@@ -0,0 +1,17 @@
+/**
+ * Created by joe on 29/05/15.
+ */
+import React from 'react';
+
+var App = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>HOMEPAGE</h1>
+
+            </div>
+        )
+    }
+});
+
+export default App;
