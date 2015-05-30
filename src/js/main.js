d51bb8067a8e104b87297dc0a0d8bf162b648977
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-05-30 11:39:50 +0100

    implementing ionic css

--- src/js/main.js
+++ src/js/main.js
@@ -22,20 +22,27 @@ var App = React.createClass({
     render() {
         return (
             <div>
-                <h1>Co-pilot</h1>
-                <RouteHandler/>
+                <div className="bar-positive bar bar-header disable-user-behavior">
+                    <h1 className="title">Ionic Styled header bar</h1>
+</div>
+                    <div className="scroll-content ionic-scroll">
+                        <div className="scroll">
+                            <RouteHandler/>
+                        </div>
+                    </div>
+
             </div>
         )
     }
 });
 
 var routes = (
-<Route handler={App} path="/">
-    <DefaultRoute handler={homePage} />
-    <Route name="about" handler={settingsPage} />
-    <Route name="crimeMapper" handler={crimeMapper} />
-    <Route name="crimeAlarm" handler={crimeAlarm} />
-</Route>
+    <Route handler={App} path="/">
+        <DefaultRoute handler={homePage} />
+        <Route name="about" handler={settingsPage} />
+        <Route name="crimeMapper" handler={crimeMapper} />
+        <Route name="crimeAlarm" handler={crimeAlarm} />
+    </Route>
 
 );
 
