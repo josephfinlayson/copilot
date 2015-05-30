17898ee3d5ca53aaa6127ba5b4399a346aef8b6a
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-05-31 00:50:10 +0100

    button bar

--- src/js/pages/health.js
+++ src/js/pages/health.js
@@ -3,8 +3,17 @@ import React from 'react';
 
 export default React.createClass({
     render() {
+        var h1Style= {textAlign: 'center'}
         return (
             <div>
+                <h1 style={h1Style} >Get medical assistance in one tap!</h1>
+
+                <div className="button-bar">
+                    <a className="button">Get insurance where you are!</a>
+                    <a className="button">Get immediate assistance!</a>
+                    <a className="button">Get insurance internationally!</a>
+                </div>
+
                 <AssistanceButton assistanceType="health" />
             </div>
         )
