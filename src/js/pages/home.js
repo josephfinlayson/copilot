5f7e978618a225d09e9f9fd3cbef8d5b3926f8eb
Ben Styles <enquiries@leeds-ebooks.co.uk>
2015-05-30 11:16:32 +0100

    Home grid working

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,14 +1,21 @@
 /**
  * Created by joe on 29/05/15.
  */
+
 import React from 'react';
+import _ from 'lodash';
 
 var Minis = React.createClass({
   render() {
     const minis = this.props.minis;
     return (
       <div>
-        {minis.map((mini, i) => (<div key={i} className="mini">{mini.name}</div>))}
+        {_.chunk(minis, 2).map((minis2, i) => (
+          <div key={i} className="row">
+            <div className="col">{minis2[0].name}</div>
+            <div className="col">{minis2[1].name}</div>
+          </div>
+        ))}
       </div>
     );
   }
@@ -17,14 +24,31 @@ var Minis = React.createClass({
 var App = React.createClass({
   render() {
     const minis = [
-      {name: 'SOS'},
-      {name: 'CrimeMapper'},
-      {name: 'example app'},
-      {name: 'example app'},
-      {name: 'example app'},
-      {name: 'example app'},
-      {name: 'example app'},
-      {name: 'example app'}
+      {
+        name: 'SOS',
+        logo: ''
+      },{
+        name: 'CrimeMapper',
+        logo: ''
+      },{
+        name: 'example app',
+        logo: ''
+      },{
+        name: 'example app',
+        logo: ''
+      },{
+        name: 'example app',
+        logo: ''
+      },{
+        name: 'example app',
+        logo: ''
+      },{
+        name: 'example app',
+        logo: ''
+      },{
+        name: 'example app',
+        logo: ''
+      }
     ];
     return (
       <div>
