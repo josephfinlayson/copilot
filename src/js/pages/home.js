4ccf21ae12ce1378132382717d0b766d5a8a9796
Ben Styles <enquiries@leeds-ebooks.co.uk>
2015-05-30 15:06:22 +0100

    Added placeholders

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -9,11 +9,11 @@ import _ from 'lodash';
 // injectTapEventPlugin();
 
 var Minis = React.createClass({
-  tap(event) {
+  handleClick(event) {
     const el = event.target,
           activated = JSON.parse(localStorage.getItem('activated-mini-apps')) || [];
     console.log(`tapped ${el.textContent}`);
-    if (confirm(`Do you want to activate ${el.textContent}?`)) {
+    if (confirm(`Do you want to ACTIVATE ${el.textContent}?`)) {
       console.log('YES');
       activated.push(el.textContent);
       localStorage.setItem('activated-mini-apps', JSON.stringify(activated));
@@ -25,48 +25,54 @@ var Minis = React.createClass({
   },
   touch(event) {
     const now = Date.now(),
-          el = event.target;
-    switch (event.type) {
-      case 'touchstart':
-        console.log('start');
+          el = event.target,
+          type = event.type;
+      if (type === 'touchstart') {
         this._touch.started = Date.now();
-        break;
-      case 'touchend':
+      } else if (type === 'touchend') {
+        const duration = this._touch.started && now - this._touch.started;
+        console.log(duration);
         event.preventDefault();
         event.stopPropagation();
-        if (this._touch.started && now - this._touch.started > 500) {
-          const activated = JSON.parse(localStorage.getItem('activated-mini-apps')) || [];
-          localStorage.setItem(
-            'activated-mini-apps',
-            JSON.stringify(_.without(activated, el.textContent))
-          );
-          console.log('hold');
+        if (duration > 500) {
+          if (confirm(`Do you want to REMOVE ${el.textContent}?`)) {
+            const activated = () => JSON.parse(localStorage.getItem('activated-mini-apps')) || [];
+            localStorage.setItem(
+              'activated-mini-apps',
+              JSON.stringify(_.without(activated(), el.textContent))
+            );
+            console.log(activated());
+          }
+        } else if (duration > 0) {
+          return this.handleClick(event);
         }
         delete this._touch.started;
-        break;
-      case 'touchcancel':
+      } else if (type === 'touchcancel') {
         delete this._touch.started;
-        break;
-      default:
-        return false;
-    }
+      } else if (type === 'touchmove') {
+        delete this._touch.started;
+      }
   },
   render() {
     const minis = this.props.minis;
     return (
       <div>
         {_.chunk(minis, 2).map((minis2, i) => (
-          <div key={i} className="row">
+          <div key={i} className="row minis">
             <div className="col"
-                 onClick={this.tap}
                  onTouchStart={this.touch}
                  onTouchEnd={this.touch}
-                 onTouchCancel={this.touch}>{minis2[0].name}</div>
+                 onTouchCancel={this.touch}
+                 onTouchMove={this.touch}>
+              <img src="http://placehold.it/300x300"/>
+            </div>
             <div className="col"
-                 onClick={this.tap}
                  onTouchStart={this.touch}
                  onTouchEnd={this.touch}
-                 onTouchCancel={this.touch}>{minis2[1].name}</div>
+                 onTouchCancel={this.touch}
+                 onTouchMove={this.touch}>
+              <img src="http://placehold.it/300x300"/>
+            </div>
           </div>
         ))}
       </div>
