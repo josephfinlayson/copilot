783700da2df414d1cf3c897eee71e96c2e2f80e5
LabsterX <labsterx@gmail.com>
2015-05-31 12:56:47 +0100

    css changes

--- src/js/pages/health.js
+++ src/js/pages/health.js
@@ -31,8 +31,6 @@ export default React.createClass({
                     assistanceType="healthInsuranceGlobal"
                 />
 
-                <AssistanceButton buttonClasses="" />
-
             </div>
         )
 
